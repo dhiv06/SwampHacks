2560cfa476080f05f0d44b5a922fbbffbef4d9c3
matilde-gillia <gilamonster.omg@gmail.com>
2026-01-23 22:30:59 -0500

    added weather.js and variables

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-import fetch from 'node-fetch';
+import { getCurrentWeather } from "./weather.js";
 
 const latitude = 29.65;
 const longitude = -82.34;
 
-const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`;
+const weather = await getCurrentWeather(latitude, longitude);
 
-async function getWeather() {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log('Weather data:', data);
-  } catch (error) {
-    console.error('Error fetching weather:', error);
-  }
-}
+console.log("Temperature:", weather.temperature);
+console.log("Condition:", weather.condition);
+console.log("Humidity:", weather.humidity);
 
-getWeather();
+// import fetch from 'node-fetch';
+
+// const latitude = 29.65;
+// const longitude = -82.34;
+
+// const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`;
+
+// async function getWeather() {
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log('Weather data:', data);
+//   } catch (error) {
+//     console.error('Error fetching weather:', error);
+//   }
+// }
+
+// getWeather();
